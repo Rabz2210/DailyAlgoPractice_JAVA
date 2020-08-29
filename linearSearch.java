2884eb42a43f70fc07dc474e47ba724f740a8c8a
Rabz2210 <topspin2210@gmail.com>
2020-08-29 23:32:34 +0530

    initial commit

--- linearSearch.java
+++ linearSearch.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class linearSearch {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int t;
-        t=scan.nextInt();
-        while(t-->0){
+        // Scanner scan = new Scanner(System.in);
+        // int t;
+        // t=scan.nextInt();
+        // while(t-->0){
             
-        }
+        // }
         
     }
 }
