2f4c27f64ed52f1a63b2a4ddca927fb891a1cb28
Rabz2210 <topspin2210@gmail.com>
2020-08-29 23:32:46 +0530

    InitialCommit

---
+++ ListImpl.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+public class ListImpl {
+    public static void main(String[] args) {
+        List<Integer> al = new ArrayList<Integer>();
+        al.add(12);
+        al.add(13);
+        Scanner scan = new Scanner(System.in);
+        scan.close();
+    }
+}
