92c213a0d3a244d5180d71798ffd31ce71d33680
Rabz2210 <topspin2210@gmail.com>
2020-08-29 23:33:15 +0530

    initial commit

---
+++ com/trees/practice/BinaryTreeFromInorderTraversal.java
@@ -0,0 +1,44 @@
+package com.trees.practice;
+
+class Node1 {
+    int data;
+    Node1 left;
+    Node1 right;
+
+    Node1(int data){
+        this.data = data;
+    }
+}
+public class BinaryTreeFromInorderTraversal {
+    static int preIndex = 0;
+    public static Node1 buildTree(int[] pre,int[] InOrder,int inStart,int inEnd){
+        if(inStart>inEnd)return null;
+        Node1 n = new Node1(pre[preIndex++]);
+        if(inStart==inEnd)return n;
+        int inIndex=0;
+        for(int i=0;i<InOrder.length;i++){
+            if(InOrder[i]==n.data)
+            {inIndex = i;}
+        }
+        n.left = buildTree(pre,InOrder,inStart,inIndex-1);
+        n.right = buildTree(pre,InOrder,inIndex+1,inEnd);
+        return n;
+    }
+    public static void inOrderTraversal(Node1 root){
+        if(root==null)return;
+        else{
+            inOrderTraversal(root.left);
+            System.out.println(""+root.data);
+            inOrderTraversal(root.right);
+        }
+    }
+    public static void main(String[] args) {
+        int[] inOrder = new int[]{40,20,50,10,30};
+        int[] preOrder = new int[]{10,20,40,50,30};
+        System.out.println("Building Tree");
+        Node1 root = buildTree(preOrder, inOrder, 0, inOrder.length-1);
+
+        System.out.println("Displaying tree");
+        inOrderTraversal(root);
+    }
+}
